939616e45f87a58c2a7bc82b2dbc2857fbfd4700
buldig <mr.karandashin@mail.ru>
2022-06-09 19:09:08 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+const item = document.querySelector(".item");
+const placeholders = document.querySelectorAll(".placeholder");
+
+item.addEventListener("dragstart", dragstart);
+item.addEventListener("dragend", dragend);
+
+placeholders.forEach((placeholder) => {
+  placeholder.addEventListener("dragover", dragover);
+  placeholder.addEventListener("dragenter", dragenter);
+  placeholder.addEventListener("dragleave", dragleave);
+  placeholder.addEventListener("drop", dragdrop);
+});
+
+function dragstart({ target }) {
+  setTimeout(() => target.classList.add("hide"), 0);
+}
+
+function dragend({ target }) {
+  target.classList.remove("hide");
+}
+
+function dragover(e) {
+  e.preventDefault();
+}
+
+function dragenter(e) {
+  e.target.classList.add("hovered");
+}
+
+function dragleave(e) {
+  e.target.classList.remove("hovered");
+}
+
+function dragdrop(e) {
+  e.target.classList.remove("hovered");
+  e.target.append(item);
+}
